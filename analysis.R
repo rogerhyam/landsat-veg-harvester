1f509b83bcc8471aab0dc81bb7a6355454c8916e
Roger Hyam <r.hyam@rbge.org.uk>
2018-03-01 21:09:07 +0000

    Introduced analysis scripts

---
+++ analysis.R
@@ -0,0 +1,15 @@
+
+# Repeatable analysis
+source('config.R')
+source('analysis_functions.R')
+
+# get a global scope db connection to use
+mydb = dbConnect(MySQL(), user=DB_USER, password=DB_PASSWORD, dbname=DB_DATABASE, host=DB_HOST)
+on.exit(dbDisconnect(mydb))
+
+ndvi_decile <- get_ndvi_by_decile('summers_14_16', 500)
+ndvi_anova <- aov(ndvi~decile, data = ndvi_decile)
+summary(ndvi_anova)
+boxplot(ndvi~decile, data = ndvi_anova)
+plot(TukeyHSD(ndvi_anova, ordered = TRUE), las=1)
+print(TukeyHSD(ndvi_anova, ordered = TRUE))
